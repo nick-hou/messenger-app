363b538a35949a4bb022b0ceb1c4dc35e3f7ba21
Nicholas Hou <nicholashou@Nicholass-MacBook-Pro.local>
2021-07-18 20:50:16 -0500

    removed async from socket creation on client

--- client/src/socket.js
+++ client/src/socket.js
@@ -9,43 +9,38 @@ import {
 } from "./store/conversations";
 var CryptoJS = require("crypto-js");
 
-const createSocket = async () => {
-  const token = await localStorage.getItem("messenger-token");
-  const socket = io(window.location.origin, {query:{token}});
+const token = localStorage.getItem("messenger-token");
+const socket = io(window.location.origin, {query:{token}});
 
-  socket.on("connect", () => {
-      console.log("connected to server");
-      socket.sendBuffer = [];
+socket.on("connect", () => {
+    console.log("connected to server");
+    socket.sendBuffer = [];
 
-      socket.on("add-online-user", (id) => {
-        store.dispatch(addOnlineUser(id));
-      });
+    socket.on("add-online-user", (id) => {
+      store.dispatch(addOnlineUser(id));
+    });
 
-      socket.on("remove-offline-user", (id) => {
-        store.dispatch(removeOfflineUser(id));
-      });
-      socket.on("new-message", (data) => {
-        const bytes  = CryptoJS.AES.decrypt(data.message, 'secret key 123');
-        const decryptedMessage = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        store.dispatch(setNewMessage(decryptedMessage, data.sender));
-      });
+    socket.on("remove-offline-user", (id) => {
+      store.dispatch(removeOfflineUser(id));
+    });
+    socket.on("new-message", (data) => {
+      const bytes  = CryptoJS.AES.decrypt(data.message, 'secret key 123');
+      const decryptedMessage = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      store.dispatch(setNewMessage(decryptedMessage, data.sender));
+    });
 
-      socket.on("read-convo", (body) => {
-        store.dispatch(readSenderConversation(body))
-      })
+    socket.on("read-convo", (body) => {
+      store.dispatch(readSenderConversation(body))
+    })
 
-      socket.on("update-typing", (body) => {
-        store.dispatch(updateTyping(body))
-      })
+    socket.on("update-typing", (body) => {
+      store.dispatch(updateTyping(body))
+    })
 
-  });
+});
 
-  socket.on("connect_error", (err) => {
-    console.log(`connect_error: ${err.message}`);
-  });
-  return socket;
-}
-
-const socket = createSocket();
+socket.on("connect_error", (err) => {
+  console.log(`connect_error: ${err.message}`);
+});
 
 export default socket;
